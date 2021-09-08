e3a293b817da4c4e2dcbdeeb3039720641cce901
AMIT <maityamit9800@gmail.com>
2021-09-08 08:48:49 +0530

    some file add

--- 0014) Halp Pyramid Using Number/Untitled1.cpp
+++ 0014) Halp Pyramid Using Number/Untitled1.cpp
@@ -4,16 +4,10 @@ int main(){
 	int n;
 	cin>>n;
 	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			if(j<=(n-i))
-			{
-				cout<<"  ";
-			}else{
-				cout<<"* ";
-			}
-		     
+		for(int j=1;j<=i;j++){
+			cout<<i;
 		}
 		cout<<endl;
 	}
-	
+	return 0;
 }
