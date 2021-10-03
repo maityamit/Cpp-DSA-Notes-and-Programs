95b2ed4aa45a654e1e11863e64d71c9131aae1d5
AMIT <maityamit9800@gmail.com>
2021-10-03 17:57:14 +0530

    some add'

---
+++ Program/0002) Kunal's Pattern Solution/Pattern 01/Untitled1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int row,col;
+	cin>>row>>col;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+	
+}
