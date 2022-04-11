d242db722730f74fb6c431a075d7ab5ef9b50e0f
Amit Maity <maityamit308@gmail.com>
2022-04-11 14:29:17 +0530

    Update sum of element.cpp

--- 004 - Recursion/Array with Recursion/sum of element.cpp
+++ 004 - Recursion/Array with Recursion/sum of element.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 
 int sum(int input[], int n) {
-  /* Don't write main().
-     Don't read input, it is passed as function argument.
-     Return output and don't print it.
-     Taking input and printing output is handled automatically.
-  */
-    static int res = 0;
-    if(n==0){
-        return 0;
-    }
-    if(n == 1 ){
-        return res+input[0];
-    }
-    res = res+input[0];
+	
+	
+//     static int res = 0;
+//     if(n==0){
+//         return 0;
+//     }
+//     if(n == 1 ){
+//         return res+input[0];
+//     }
+//     res = res+input[0];
     
-    return sum(input+1,n-1);
+//     return sum(input+1,n-1);
+	
+    if(n==0)
+        return 0;
+    int ans = sum(input+1,n-1);
+    return ans+input[0];
     
 
 }
