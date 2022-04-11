0ca938f85436e8bf5ef61e0d8dda26db3c0ec169
Amit Maity <maityamit308@gmail.com>
2022-04-11 14:36:12 +0530

    Update check number present or not.cpp

--- 004 - Recursion/Array with Recursion/check number present or not.cpp
+++ 004 - Recursion/Array with Recursion/check number present or not.cpp
@@ -2,22 +2,29 @@
 using namespace std;
 
 bool checkNumber(int input[], int size, int x) {
-  /* Don't write main().
-     Don't read input, it is passed as function argument.
-     Return output and don't print it.
-     Taking input and printing output is handled automatically.
-  */
+
     
+//     if(size==0){
+//         return false;
+//     }
+//     if(size == 1 && input[0]==x){
+//         return true;
+//     }
+//     if(input[0]==x){
+//         return true;
+//     }
+//     return checkNumber(input+1,size-1,x);
+	
+	
     if(size==0){
         return false;
     }
-    if(size == 1 && input[0]==x){
-        return true;
-    }
+    bool res = checkNumber(input+1,size-1,x);
     if(input[0]==x){
         return true;
+    }else{
+        return res;
     }
-    return checkNumber(input+1,size-1,x);
 
 }
 
