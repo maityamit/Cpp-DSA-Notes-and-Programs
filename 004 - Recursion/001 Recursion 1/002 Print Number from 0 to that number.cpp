73274abf8bfa3ede97abd4dbb3963da06f3cfc7a
Amit Maity <maityamit308@gmail.com>
2022-04-16 00:50:31 +0530

    Update 002 Print Number from 0 to that number.cpp

--- 004 - Recursion/001 Recursion 1/002 Print Number from 0 to that number.cpp
+++ 004 - Recursion/001 Recursion 1/002 Print Number from 0 to that number.cpp
@@ -1,3 +1,4 @@
+//Given is the code to print numbers from 1 to n in increasing order recursively. But it contains few bugs that you need to rectify such that all the test cases pass.
 #include<iostream>
 using namespace std;
 void print(int n){
