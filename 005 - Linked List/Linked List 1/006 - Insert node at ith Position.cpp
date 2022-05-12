00ad3b81f25474697049c479cfdebaab33f37dc6
maityamit <maityamit308@gmail.com>
2022-05-13 03:24:33 +0530

    kl

---
+++ 005 - Linked List/Linked List 1/006 - Insert node at ith Position.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include "node.cpp"
+using namespace std;
+
+// Time Complexity of this Fn. is O(n)-> 
+Node *takeInput2(){
+	int data;
+	cin>>data;
+	Node *head=NULL;
+	Node *tail=NULL;
+	while(data != -1){
+		//Here we declare dynamic type
+		//caz if we declare statically we
+		//can loose our data
+		Node *n = new Node(data);
+		if(head==NULL){
+			head=n;
+			tail=n;
+		}else{
+			tail->next = n;
+			tail = tail->next;
+		}
+		cin>>data;
+	}
+	return head;
+}
+Node* insertNode(Node *head,int position , int data){
+	Node *n = new Node(data);
+	if(position==0){
+		n->next = head;
+		head = n;
+		return head;
+	}
+	int count = 0;
+	Node *temp = head;
+	while(temp!= NULL && count<position-1){
+		temp = temp->next;
+		count++;
+	}
+	if(temp!=NULL){
+		n->next = temp->next;
+		temp->next = n;
+	}
+	return head;
+	
+}
+void print(Node *head){
+	//Sure make a Temporary Node Pointer to make sure not to lost head
+	Node *temp = head;
+	while(temp != NULL){
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+	cout<<endl;
+}
+int main(){
+	
+	Node *head = takeInput2();
+	print(head);
+	head = insertNode(head,0,99);
+	print(head);
+	
+	return 0;
+}
