e1f49be351c4084ca554f305601a4245f7b13b4c
maityamit <maityamit308@gmail.com>
2022-05-14 12:02:54 +0530

    Add

---
+++ 005 - Linked List/Linked List 1/000X - Remove duplicates.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+Node *removeDuplicates(Node *head)
+{
+    //Write your code here
+    Node *temp = head;
+    while(temp!=NULL){
+        if(temp->data == (temp->next)->data){
+            Node *hello = temp->next;
+            temp->next = (temp->next)->next;
+            delete hello;
+        }
+    }
+    return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		head = removeDuplicates(head);
+		print(head);
+	}
+	return 0;
+}
