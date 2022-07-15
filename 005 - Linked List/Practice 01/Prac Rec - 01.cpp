b7c849adf19abeeda2674484f932ae96aa745e07
maityamit <maityamit308@gmail.com>
2022-07-16 00:58:23 +0530

    add

--- 005 - Linked List/Practice 01/Prac Rec - 01.cpp
+++ 005 - Linked List/Practice 01/Prac Rec - 01.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 class Node{
 	public:
@@ -248,9 +249,289 @@ bool palindromeLL(Node* head){
 	return true;
 }
 
+//Mid of a LL
+int midofLL(Node* head){
+	if(head==NULL || head->next==NULL){
+		return -1;
+	}
+	Node* slow = head;
+	Node* fast = head->next;
+	while(fast!=NULL && fast->next!=NULL){
+		slow=slow->next;
+		fast=fast->next->next;
+	}
+	return slow->data;
+}
+
+//Merge two sorted Linked List
+Node* mergeTwoSorted(Node* h1,Node* h2){
+	Node* head = NULL;
+	Node* tail = NULL;
+	Node* head1=h1;
+	Node* head2=h2;
+	while(head1!=NULL && head2!=NULL){
+		if(head1->data>head2->data){
+			Node* temp = new Node(head2->data);
+			if(head==NULL){
+				head=temp;
+				tail=temp;
+			}else{
+				tail->next=temp;
+				tail=tail->next;
+			}
+			head2=head2->next;
+		}else{
+			Node* temp = new Node(head1->data);
+			if(head==NULL){
+				head=temp;
+				tail=temp;
+			}else{
+				tail->next=temp;
+				tail=tail->next;
+			}
+			head1=head1->next;
+		}
+	}
+	while(head1!=NULL){
+		tail->next=head1;
+		tail=tail->next;
+		head1=head1->next;
+	}
+	while(head2!=NULL){
+		tail->next=head2;
+		tail=tail->next;
+		head2=head2->next;
+	}
+	return head;
+}
+
+//Reverse LL Recusrsively
+Node* reverseLLRec(Node* head){
+	if(head==NULL || head->next==NULL){
+		return head;
+	}
+	Node* temp = reverseLLRec(head->next);
+	Node* curr = temp;
+	while(curr->next!=NULL){
+		curr=curr->next;
+	}
+	curr->next = head;
+	head->next = NULL;
+	return temp;
+}
+
+//Reverse LL Iteratively
+Node* reverseLLIte(Node* head){
+	Node* prev=NULL;
+	Node* curr=head;
+	if(head==NULL || head->next==NULL){
+		return head;
+	}
+	Node* next = NULL;
+	while(curr!=NULL){
+		next=curr->next;
+		curr->next=prev;
+		prev=curr;
+		curr=next;
+	}
+	return prev;
+}
+
+//Find a Node in LL
+int findaNodeinLL(Node* head,int val){
+	if(head==NULL){
+		return -1;
+	}
+	int temp = findaNodeinLL(head->next,val);
+	if(head->data==val){
+		return 0;
+	}else{
+		if(temp==-1){
+			return temp;
+		}else{
+			return temp+1;
+		}
+	}
+}
+
+//Even after odd LL
+Node* evenAfterOddLL(Node* head){
+	 if(head==NULL || head->next==NULL){
+        return head;
+    }
+    
+    queue<Node*> even;
+	queue<Node*> odd;
+	Node* temp = head;
+	while(temp!=NULL){
+		if(temp->data%2==0){
+			even.push(temp);
+		}else{
+			odd.push(temp);
+		}
+		temp=temp->next;
+	}
+	Node* h = NULL;
+	Node* tail=NULL;
+	while(odd.size()!=0){
+		if(h==NULL){
+			h=odd.front();
+			tail=odd.front();
+			odd.pop();
+		}else{
+			tail->next=odd.front();
+			tail=tail->next;
+			odd.pop();
+		}
+	}
+	while(even.size()!=0){
+		if(h==NULL){
+			h=even.front();
+			tail=even.front();
+			even.pop();
+		}else{
+			tail->next=even.front();
+			tail=tail->next;
+			even.pop();
+		}
+	}
+	return h;
+}
+
+Node* skipMdeleteN(Node* head,int M,int N){
+	if(head==NULL){
+		return head;
+	}
+	if(M==0){
+		return NULL;
+	}
+	if(N==0){
+		return head;
+	}
+	Node* temp = head;
+	int c1=1,c2=1;
+	while(temp!=NULL){
+		while(c1!=M && temp!=NULL){
+			temp=temp->next;
+			c1++;
+		}
+		if(temp==NULL){
+			return head;
+		}
+		Node* amit = temp->next;
+		while(c2!=N && amit!=NULL){
+			amit=amit->next;
+			c2++;
+		}
+		c1=1;c2=1;
+		if(amit!=NULL){
+		  temp->next=amit->next;
+		  temp=temp->next;	
+		}else{
+		  temp->next=NULL;
+		}
+	}
+	return head;
+}
+
+//Swap two Nodes in LL
+Node* swapTwoNodes(Node* head,int m,int n){
+	if(m==0 && n-m==1){
+	    Node* c1=head;
+		Node* c2 = c1->next;
+		Node* p2 = c2->next;
+		head=c2;
+		c2->next=c1;
+		c1->next=p2;
+		return head;
+	}else if(m==0){
+		Node* c1=head;
+		Node* p2 = head;
+		Node* c2 = head;
+		while(n>1){
+			p2=p2->next;
+			n--;
+		}
+		Node* tt = head->next;
+		c2=p2->next;
+		p2->next=head;
+		head->next=c2->next;
+		c2->next=p2;
+		head=c2;
+		head->next=tt;
+		return head;
+	}else if(n-m==1){
+		Node* p1=head;
+		Node* p2=head;
+		Node* c1=head;
+		Node* c2=head;
+		while(m>1){
+			p1=p1->next;
+			m--;
+		}
+		c1=p1->next;
+		c2=c1->next;
+		p2=c2->next;
+		p1->next=c2;
+		c2->next=c1;
+		c1->next=p2;
+		return head;
+	}else{
+		Node* p1=head;
+		Node* p2=head;
+		Node* c1=head;
+		Node* c2=head;
+		while(m>1){
+			p1=p1->next;
+			m--;
+		}
+		c1=p1->next;
+		while(n>1){
+			p2=p2->next;
+			n--;
+		}
+		c2=p2->next;
+		p1->next=c2;
+		p2->next=c1;
+		c1->next=c2->next;
+		c2->next=p2;
+		return head;
+	}
+}
+
+//kReverse of the Linked List
+Node* kReverse(Node* head,int k){
+	if(head==NULL){
+		return head;
+	}
+	Node* temp = head;
+	int t = k;
+	while(t>1 && temp!=NULL){
+		temp=temp->next;
+		t--;
+	}
+	Node* amit;
+	if(temp!=NULL){
+        amit = temp->next;
+    	temp->next=NULL;		
+    }else{
+    	amit=NULL;
+	}
+	Node* yash = reverseLLRec(head);
+	Node* tt = kReverse(amit,k);
+	Node* vishal = yash;
+	while(yash->next!=NULL){
+		yash=yash->next;
+	}
+	yash->next=tt;
+	return vishal;
+}
+
+
 int main(){
 	//10 20 30 40 50 60 70 80 90 100
 	Node* head = takeInput();
+//	Node* head2 = takeInput();
 	printLinkedList(head);
 //	cout<<"\n"<<"LinkedList Length : "<<lengthLL(head)<<endl;
 //	printIthNode(head,10);
@@ -263,6 +544,15 @@ int main(){
 //  printLinkedList(appendLastN(head,3));
 //  printLinkedList(eliminateDupli(head));
 //  reverseLLPrint(head);
-    cout<<palindromeLL(head)<<endl;
+//  cout<<palindromeLL(head)<<endl;
+//  cout<<midofLL(head)<<endl;
+//  printLinkedList(mergeTwoSorted(head,head2));
+//  printLinkedList(reverseLLRec(head));
+//  printLinkedList(reverseLLIte(head));
+//  cout<<findaNodeinLL(head,30);
+//  printLinkedList(evenAfterOddLL(head));
+//  printLinkedList(skipMdeleteN(head,2,2));
+//  printLinkedList(swapTwoNodes(head,2,4));
+    printLinkedList(kReverse(head,3));
 	return 0;
 }
