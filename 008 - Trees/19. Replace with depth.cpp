7411454a7c7cea41b06ea204920cdb711292fd7f
Amit Maity <maityamit308@gmail.com>
2022-09-21 00:39:32 +0530

    Create 19. Replace with depth.cpp

---
+++ 008 - Trees/19. Replace with depth.cpp
@@ -0,0 +1,36 @@
+/************************************************************
+
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+
+        TreeNode(T data) {
+            this->data = data;
+        }
+
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+void help(TreeNode<int> *root, int depth)
+{
+    root->data=depth;
+    for(int i=0;i<root->children.size();i++)
+    {
+        help(root->children[i] ,depth+1);
+    }
+}
+void replaceWithDepthValue(TreeNode<int>* root) {
+    // Write your code here
+    
+    int depth=0;
+ help(root,depth);
+}
